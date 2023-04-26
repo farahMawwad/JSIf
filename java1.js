1ce35b3fecaf0891f8234b6d68f0d637bdb12437
FARAH <farah.moh.awwad@gmail.com>
2023-04-27 00:15:15 +0300

    edit

---
+++ java1.js
@@ -0,0 +1,72 @@
+let num =24;
+if (num>18){
+    console.log("You are an adult")
+}
+let num_1 =45;
+if (num<50){
+    console.log("You failed the test")
+}
+let string="john";
+if (string=="john"){
+    console.log("Hello "+string)
+}
+let string_1="Monday";
+if (string_1=="Monday"){
+    console.log("It's a weekday")
+}
+let number=4;
+if (number%2==0){
+    console.log('The number is even')
+}
+let char='a';
+if (char>= 'a'){
+    console.log('It\'s a lette')
+}
+let list=[2,3,5];
+if (Array.isArray(list)){
+    console.log("It's an array")
+}
+let x=5;
+if (x>0){
+    console.log("x is a positive number")
+}
+let y=-5;
+if (y<0){
+    console.log("y is a negative number")
+}
+let z=9;
+if (z%3==0){
+    console.log('z is a multiple of')
+}
+let gpa=3.5;
+if (gpa>3.0){
+    console.log("Congratulations, you have a good GPA!")
+}
+let password ='mypassword123';
+if (password.length>8){
+    console.log("Your password is strong")
+}
+let age =30;
+if (age>18 && age<65){
+    console.log("You are of working age")
+}
+let color ="red";
+if (color=="red" || color=="blue" || color=='green'){
+    console.log("color is a primary color")
+}
+let n1=11;
+if (isNaN(n1)){}
+else{console.log('11 is a valid number')}
+
+let n2="19";
+if (isNaN(n2)){}
+else{console.log('19 is a valid number')}
+let n3="xyz";
+if (isNaN(n3)){console.log("xyz is not a number")}
+else{}
+let n4="7.5";
+if (isNaN(n4)){}
+else{console.log('7.5 is a valid number')}
+let n5="21F";
+if (isNaN(n5)){console.log("21F is not a number")}
+else{}
